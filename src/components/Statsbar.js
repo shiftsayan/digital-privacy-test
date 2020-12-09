9313abe5176cea88a48785588d8c658ace63f473
Joseph Zhang <josephzhang@cmu.edu>
2020-12-09 06:56:14 -0800

    add carrot triangle on stats page

--- src/components/Statsbar.js
+++ src/components/Statsbar.js
@@ -8,10 +8,19 @@ export default function Statsbar(props) {
       <h5>{props.prompt}</h5>
 
       <div className={styles.outerBar}>
-        { props.your && <span className={styles.user} style={{left: `${props.your}%`}}>{props.your_label}</span> } 
-        <span className={styles.innerBar} style={{width: `${props.mean}%`}}>
-          <span className={styles.average}>{props.mean_label}</span>
+        {props.your && (
+          <span className={styles.user} style={{ left: `${props.your}%` }}>
+            {props.your_label}
+            <div className={`${styles.arrowDown} ${styles.arrowBlack}`}></div>
+          </span>
+        )}
+        <span className={styles.innerBar} style={{ width: `${props.mean}%` }}>
+          <span className={styles.average}>
+            {props.mean_label}
+            <div className={`${styles.arrowDown} ${styles.arrowBlue}`}></div>
+          </span>
         </span>
+
         <p className={styles.leftTag}>{props.left_tag}</p>
         <p className={styles.rightTag}>{props.right_tag}</p>
       </div>
